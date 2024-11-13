170d3f309cab9c50e9af153a6d3897c64eaaa073
SamuelForbes <samuelforbes@live.com>
2024-11-13 20:38:11 +0000

    added option for valid file format types

--- src/app/args.rs
+++ src/app/args.rs
@@ -10,4 +10,8 @@ pub struct Args {
     ///The path to the folder you would like them sorted to
     #[arg(short, long= "target", default_value = ".")]
     pub target_directory: String,
+    
+    ///A comma seperated list of file formats that are considered valid to check and move
+    #[arg(short, long="valid-file-formats", value_parser, num_args=1.. , value_delimiter=',', default_value="jpeg,jpg,png")]
+    pub valid_file_formats: Vec<String>,
 }
