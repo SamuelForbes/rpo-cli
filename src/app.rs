880cbb102541fa03840613498633724f38c7b7c8
SamuelForbes <samuelforbes@live.com>
2024-11-13 20:19:31 +0000

    primitive error handling

--- src/app.rs
+++ src/app.rs
@@ -4,51 +4,46 @@ mod file_move;
 use clap::Parser;
 use args::Args;
 use std::collections::HashSet;
-use std::fs;
+use std::{fs, io};
 use std::path::Path;
 use file_move::*;
 use rayon::prelude::*;
 
-pub fn run(){
+pub fn run() {
     if let Err(error) = move_photos(Args::parse()) {
         eprintln!("{error}");
         std::process::exit(1);
     }
 }
 
-fn move_photos(args: Args) -> Result<(), String>{
+fn move_photos(args: Args) -> Result<(), io::Error> {
     println!("{:?}", args);
 
-    let file_moves = build_file_moves(&*args.source_directory);
-
+    let file_moves = build_file_moves(&*args.source_directory)?;
+    
     create_new_folders(&file_moves, &args.target_directory)?;
     process_moves(&file_moves, &*args.target_directory);
 
     Ok(())
 }
 
-fn build_file_moves(source_path: &str) -> Vec<FileMove> {
-    let result = fs::read_dir(source_path);
-
-    match result {
-        Ok(read_dir) => {
-            read_dir.filter(|entry| entry.path().is_file())
-                .map(FileMove::from)
-                .collect::<Vec<FileMove>>()
-        }
-        Err(error) => {
-            eprintln!("{error}");
-        }
+fn build_file_moves(source_path: &str) -> Result<Vec<FileMove>, io::Error> {
+    let mut moves = vec!();
+    
+    for entry in fs::read_dir(source_path)? {
+        moves.push(FileMove::from(entry?))
     }
+    
+    Ok(moves)
 }
 
 fn process_moves(file_moves: &Vec<FileMove>, target_path: &str) {
     file_moves
         .par_iter()
         .for_each(|file_move| {
-            let file_name = file_move.source.file_name().into_string().unwrap().replace("\"","");
+            let file_name = file_move.source.file_name().into_string().unwrap().replace("\"", "");
 
-            let target =  if file_move.target_path.is_some() {
+            let target = if file_move.target_path.is_some() {
                 format!("{}{}/{}", target_path, file_move.target_path.as_ref().unwrap(), file_name)
             } else {
                 format!("{}/{}", target_path.to_string(), file_name)
@@ -57,15 +52,14 @@ fn process_moves(file_moves: &Vec<FileMove>, target_path: &str) {
                 Err(e) => eprintln!("{e}"),
                 _ => ()
             }
-
         });
 }
 
-fn create_new_folders(file_moves: &Vec<FileMove>, target_path: &str) -> Result<(), Err> {
+fn create_new_folders(file_moves: &Vec<FileMove>, target_path: &str) -> Result<(), io::Error> {
     let mut required_folders = HashSet::new();
 
     for file_move in file_moves {
-        if file_move.target_path.as_ref().is_some(){
+        if file_move.target_path.as_ref().is_some() {
             required_folders.insert(file_move.target_path.as_ref().unwrap());
         }
     }
@@ -75,11 +69,11 @@ fn create_new_folders(file_moves: &Vec<FileMove>, target_path: &str) -> Result<(
         let year_folder = format!("{target_path}/{}", path_pieces[0]);
         let month_folder = format!("{target_path}{entry}");
 
-        if !Path::new(&year_folder).exists(){
+        if !Path::new(&year_folder).exists() {
             fs::create_dir(year_folder)?;
         }
 
-        if !Path::new(&month_folder).exists(){
+        if !Path::new(&month_folder).exists() {
             fs::create_dir(month_folder)?;
         }
     }
