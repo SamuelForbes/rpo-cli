d80fd10e80ba8316ac3493a8bfbde058a31a7133
SamuelForbes <samuelforbes@live.com>
2024-11-21 20:50:31 +0000

    added check for file types

--- src/app.rs
+++ src/app.rs
@@ -5,6 +5,7 @@ use clap::Parser;
 use args::Args;
 use std::collections::HashSet;
 use std::{fs, io};
+use std::fs::DirEntry;
 use std::path::Path;
 use file_move::*;
 use rayon::prelude::*;
@@ -19,22 +20,36 @@ pub fn run() {
 fn move_photos(args: Args) -> Result<(), io::Error> {
     println!("{:?}", args);
 
-    let file_moves = build_file_moves(&*args.source_directory)?;
-    
+    let file_moves = build_file_moves(&args)?;
+
     create_new_folders(&file_moves, &args.target_directory)?;
     process_moves(&file_moves, &*args.target_directory);
 
     Ok(())
 }
 
-fn build_file_moves(source_path: &str) -> Result<Vec<FileMove>, io::Error> {
+fn build_file_moves(args: &Args) -> Result<Vec<FileMove>, io::Error> {
     let mut moves = vec!();
-    
-    for entry in fs::read_dir(source_path)? {
-        moves.push(FileMove::from(entry?))
+
+    for result in fs::read_dir(args.source_directory.clone())? {
+        let entry = result?;
+        if entry_is_valid_file_type(&args, &entry) {
+            moves.push(FileMove::from(entry))
+        }
     }
-    
+
     Ok(moves)
+}
+
+fn entry_is_valid_file_type(args: &Args, entry: &DirEntry) -> bool {
+    args.valid_file_formats.contains(
+        &entry.file_name().into_string()
+            .expect(format!("{:?} is a bad file name", entry.file_name()).as_str())
+            .split('.')
+            .last()
+            .expect(format!("{:?} file does not have a suffix", entry.file_name()).as_str())
+            .to_string()
+    )
 }
 
 fn process_moves(file_moves: &Vec<FileMove>, target_path: &str) {
@@ -57,8 +72,8 @@ fn process_moves(file_moves: &Vec<FileMove>, target_path: &str) {
 
 fn create_new_folders(file_moves: &Vec<FileMove>, target_path: &str) -> Result<(), io::Error> {
     let mut required_folders = HashSet::new();
-    
-   create_path_if_not_exists(String::from(target_path))?;
+
+    create_path_if_not_exists(String::from(target_path))?;
 
     for file_move in file_moves {
         if file_move.target_path.as_ref().is_some() {
@@ -70,7 +85,7 @@ fn create_new_folders(file_moves: &Vec<FileMove>, target_path: &str) -> Result<(
         let path_pieces = entry[1..].split("/").collect::<Vec<&str>>();
         let year_folder = format!("{target_path}/{}", path_pieces[0]);
         let month_folder = format!("{target_path}{entry}");
-        
+
         create_path_if_not_exists(year_folder)?;
         create_path_if_not_exists(month_folder)?;
     }
@@ -82,6 +97,28 @@ fn create_path_if_not_exists(path_string: String) -> Result<(), io::Error> {
     if !Path::new(&path_string).exists() {
         fs::create_dir(path_string)?;
     }
-    
+
     Ok(())
+}
+
+#[test]
+fn valid_file_formats(){
+    let args = Args {
+        source_directory: "test".to_string(),
+        target_directory: "test".to_string(),
+        valid_file_formats: vec!("jpg".to_string())
+    };
+    
+    assert!(entry_is_valid_file_type(&args, &fs::read_dir("./Test").unwrap().map(|entry| entry.unwrap()).collect::<Vec<DirEntry>>()[0]));
+}
+
+#[test]
+fn invalid_file_formats(){
+    let args = Args {
+        source_directory: "test".to_string(),
+        target_directory: "test".to_string(),
+        valid_file_formats: vec!("png".to_string())
+    };
+
+    assert!(!entry_is_valid_file_type(&args, &fs::read_dir("./Test").unwrap().map(|entry| entry.unwrap()).collect::<Vec<DirEntry>>()[0]));
 }
